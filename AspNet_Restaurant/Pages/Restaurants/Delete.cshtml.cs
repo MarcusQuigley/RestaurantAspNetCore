df12c450dd2f2bb42d3348305aa20dfecd6c83be
marcus quigley <marcusquigley@gmail.com>
2019-12-08 09:04:04 -0500

    delete restaurant

---
+++ AspNet_Restaurant/Pages/Restaurants/Delete.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNet_Restaurant.Core;
+using AspNet_Restaurant.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNet_Restaurant
+{
+    public class DeleteModel : PageModel
+    {
+        
+        readonly            IRestaurantDataService service;
+
+        public DeleteModel(IRestaurantDataService service)
+        {
+            this.service = service;
+        }
+
+        [BindProperty]
+        public Restaurant Restaurant { get; set; }
+
+        public IActionResult OnGet(int restaurantId) {
+
+            Restaurant = service.GetById(restaurantId);
+            if (Restaurant==null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int restaurantId)
+        {
+            Restaurant = service.Delete(restaurantId);
+                if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+         //   service.Commit();
+
+            return RedirectToPage("List");
+        }
+
+        
+    }
+}
